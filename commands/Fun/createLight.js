8b0d17bd63fb9c23801d7b3ba4c7dcf9b2f165a3
flex5hybrid <1kazak2dimon@mail.ru>
2023-09-23 21:06:46 +0300

    first commit

---
+++ commands/Fun/createLight.js
@@ -0,0 +1,137 @@
+const { ActionRowBuilder, SlashCommandBuilder, ButtonBuilder, ButtonStyle, ComponentType} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('createlight')
+		.setDescription('Создаёт светофор'),
+	async execute(interaction) {
+        const kassi = new ButtonBuilder()
+        .setCustomId('kassi')
+        .setLabel('Кассиопея')
+        .setStyle(ButtonStyle.Danger);
+
+        const eridan = new ButtonBuilder()
+        .setCustomId('eridan')
+        .setLabel('Эридан')
+        .setStyle(ButtonStyle.Danger);
+
+        const cepheus = new ButtonBuilder()
+        .setCustomId('cepheus')
+        .setLabel('Цефей')
+        .setStyle(ButtonStyle.Danger);
+
+        const row = new ActionRowBuilder()
+	    .addComponents(kassi, eridan, cepheus);
+
+        const response = await interaction.reply({
+			content: `На каком вы сервере?`,
+			components: [row],
+		});
+        
+
+        const collectorFilter = function(i) {
+            return i.user.id;
+        }
+        const collector = response.createMessageComponentCollector({
+            componentType: ComponentType.Button
+        })
+        let kassiAdmins = [];
+        let eridanAdmins = [];
+        let cepheusAdmins = [];
+        let replyKassi = ' ';
+        let replyEridan = ' ';
+        let replyCepheus = ' ';
+        let used = false;
+        let msgRef;
+        collector.on('collect', async (interaction) => {
+        
+           userclick = interaction.user.id;
+            // if the kassi button is clicked
+            if (interaction.customId === 'kassi') {
+                let deletedKassi = false;
+                for (let a = 0; a < kassiAdmins.length; a++) {
+                    if (kassiAdmins[a] === userclick) {
+                        kassiAdmins.splice(a, 1);
+                        deletedKassi = true;
+                    }
+                }
+                if (deletedKassi == false) {
+                    kassiAdmins.push(userclick);
+                }
+            } 
+            // if the eridan button is clicked
+            if (interaction.customId === 'eridan') {
+                let deletedEridan = false;
+                for (let a = 0; a < eridanAdmins.length; a++) {
+                    if (eridanAdmins[a] === userclick) {
+                        eridanAdmins.splice(a, 1);
+                        deletedEridan = true;
+                    }
+                }
+                if (deletedEridan == false) {
+                    eridanAdmins[eridanAdmins.length] = userclick;
+                }
+                
+            }
+            // if the cepheus button is clicked
+            if (interaction.customId === 'cepheus') {
+                let deletedCepheus = false;
+                for (let a = 0; a < cepheusAdmins.length; a++) {
+                    if (cepheusAdmins[a] === userclick) {
+                        cepheusAdmins.splice(a, 1);
+                        deletedCepheus = true;
+                    }
+                }
+                if (deletedCepheus == false) {
+                    cepheusAdmins[cepheusAdmins.length] = userclick;
+                }
+                
+            }
+
+
+
+            // checking for kassi massive
+            if (kassiAdmins[0]) {
+                for (let a = 0; a < kassiAdmins.length; a++) {
+                    replyKassi = replyKassi + `<@${kassiAdmins[a]}>` + ' ';
+                }
+            } else {
+                replyKassi = "Нет"
+            }
+
+            // checking for eridan massive
+            if (eridanAdmins[0]) {
+                for (let a = 0; a < eridanAdmins.length; a++) {
+                    replyEridan = replyEridan + `<@${eridanAdmins[a]}>` + ' ';   
+                }
+            } else {
+                replyEridan = "Нет"
+            }
+            // checking for cepheus massive
+            if (cepheusAdmins[0]) {
+                for (let a = 0; a < cepheusAdmins.length; a++) {
+                    replyCepheus = replyCepheus + `<@${cepheusAdmins[a]}>` + ' ';   
+                }
+            } else {
+                replyCepheus = "Нет"
+            }
+
+            if (used == false) {
+                msgRef = await interaction.reply(`**Кассиопея:** ${replyKassi} \n**Эридан:** ${replyEridan} \n**Цефей:** ${replyCepheus}`);
+            }
+            else {
+                msgRef.edit(`**Кассиопея:** ${replyKassi} \n**Эридан:** ${replyEridan} \n**Цефей:** ${replyCepheus}`);
+                interaction.deferUpdate();
+            }
+            
+            replyKassi = '';
+            replyEridan = '';
+            replyCepheus = '';
+            used = true;
+
+
+            
+        })
+        
+    }
+}
